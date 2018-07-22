33a049c5207aef5fa33477f8c82c5508474171a9
jay <vanjaydo@gmail.com>
2018-07-22 15:31:23 +0800

    feat(Authority): 定义权限种类与权限表;

---
+++ src/main/java/cn/edu/upc/eduroamcontrolsystembackend/security/model/Authority.java
@@ -0,0 +1,51 @@
+package cn.edu.upc.eduroamcontrolsystembackend.security.model;
+
+import cn.edu.upc.eduroamcontrolsystembackend.model.User;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * 定义用户权限的表
+ *
+ * @author jay
+ * @date 2018/07/22
+ */
+
+@Entity
+@Table(name = "authority")
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private AuthorityType authorityType;
+
+    @ManyToMany(mappedBy = "authorities", fetch = FetchType.LAZY)
+    private List<User> users;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public AuthorityType getAuthorityType() {
+        return authorityType;
+    }
+
+    public void setAuthorityType(AuthorityType authorityType) {
+        this.authorityType = authorityType;
+    }
+}
---
+++ src/main/java/cn/edu/upc/eduroamcontrolsystembackend/security/model/AuthorityType.java
@@ -0,0 +1,12 @@
+package cn.edu.upc.eduroamcontrolsystembackend.security.model;
+
+/**
+ * 定义权限种类, 分为普通用户与管理员
+ *
+ * @author jay
+ * @date 2018/07/22
+ */
+
+public enum AuthorityType {
+    ROLE_USER, ROLE_ADMIN
+}
