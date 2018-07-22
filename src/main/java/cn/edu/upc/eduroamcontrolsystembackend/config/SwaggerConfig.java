d0962bb0e3eb92beb93297d47e6f9675431bbf5a
jay <vanjaydo@gmail.com>
2018-07-22 16:24:11 +0800

    feat(swagger config): 添加swagger配置;

---
+++ src/main/java/cn/edu/upc/eduroamcontrolsystembackend/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package cn.edu.upc.eduroamcontrolsystembackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 配置Swagger2
+ *
+ * @author jay
+ * @date 2018/07/21
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.edu.upc.eduroamcontrolsystembackend.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("eduroam control system API")
+                .termsOfServiceUrl("https://github.com/UPC-eduroam")
+                .version("1.0")
+                .build();
+    }
+
+}
