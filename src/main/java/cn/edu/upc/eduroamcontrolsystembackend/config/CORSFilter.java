1fb3379b595bd4483585042582f1754f58404cb4
jay <vanjaydo@gmail.com>
2018-07-27 12:00:27 +0800

    feat(CORS filter): 添加跨域的设置;

---
+++ src/main/java/cn/edu/upc/eduroamcontrolsystembackend/config/CORSFilter.java
@@ -0,0 +1,39 @@
+package cn.edu.upc.eduroamcontrolsystembackend.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.*;
+
+/**
+ * Created by jay on 2018/07/27
+ */
+
+@Component
+public class CORSFilter implements javax.servlet.Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
