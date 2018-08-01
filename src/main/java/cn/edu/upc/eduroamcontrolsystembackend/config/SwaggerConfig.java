e386c8db58a0ac8862efd21c381ea472d22644d6
jay <vanjaydo@gmail.com>
2018-08-02 00:40:25 +0800

    feat(swagger config): 更新swagger配置, 全局增加header中的token字段;

--- src/main/java/cn/edu/upc/eduroamcontrolsystembackend/config/SwaggerConfig.java
+++ src/main/java/cn/edu/upc/eduroamcontrolsystembackend/config/SwaggerConfig.java
@@ -3,12 +3,18 @@ package cn.edu.upc.eduroamcontrolsystembackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 配置Swagger2
@@ -28,7 +34,8 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("cn.edu.upc.eduroamcontrolsystembackend.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(setHeaderToken()); //设置自动全局添加token认证字段
     }
 
     private ApiInfo apiInfo() {
@@ -37,6 +44,14 @@ public class SwaggerConfig {
                 .termsOfServiceUrl("https://github.com/UPC-eduroam")
                 .version("1.0")
                 .build();
+    }
+
+    private List<Parameter> setHeaderToken() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("Authorization").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        return pars;
     }
 
 }
