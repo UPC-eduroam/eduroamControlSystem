27f37f22372742d9f060fe71c9fc6daa744607f5
jay <vanjaydo@gmail.com>
2018-08-24 14:59:59 +0800

    feat(AESCrypt): 新增AES加密解密工具类AESCrypt;

---
+++ src/main/java/cn/edu/upc/eduroamcontrolsystembackend/util/AESCrypt.java
@@ -0,0 +1,72 @@
+package cn.edu.upc.eduroamcontrolsystembackend.util;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.security.Key;
+import java.security.SecureRandom;
+
+/**
+ * AES加密解密工具类
+ * 利用AES对称加密算法对数据进行加密与解密
+ *
+ * Created by jay on 2018/08/23
+ */
+
+public class AESCrypt {
+
+    private static String defaultKeySeed = "a2:!s8d]14[";
+
+    public static String encrypt(String plainText) {
+        Key secretKey = getKey(null);
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] p = plainText.getBytes("UTF-8");
+            byte[] result = cipher.doFinal(p);
+            BASE64Encoder encoder = new BASE64Encoder();
+            return encoder.encode(result);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String decrypt(String cipherText) {
+        Key secretKey = getKey(null);
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            BASE64Decoder decoder = new BASE64Decoder();
+            byte[] c = decoder.decodeBuffer(cipherText);
+            byte[] result = cipher.doFinal(c);
+            return new String(result, "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Key getKey(String keySeed) {
+        if (keySeed == null) {
+            keySeed = System.getenv("AES_SYS_KEY");
+        }
+        if (keySeed == null) {
+            keySeed = System.getProperty("AES_SYS_KEY");
+        }
+        if (keySeed == null || keySeed.trim().length() == 0) {
+            keySeed = defaultKeySeed;
+        }
+        try {
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+            secureRandom.setSeed(keySeed.getBytes());
+            KeyGenerator generator = KeyGenerator.getInstance("AES");
+            generator.init(secureRandom);
+            return generator.generateKey();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
